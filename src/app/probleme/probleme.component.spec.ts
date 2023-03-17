babb9bc45aae283dd3d5125c50cb00d96c1add1d
Mathieu Girard <mgirard514@gmail.com>
2023-03-17 14:48:22 -0400

    Ajout du test #1

--- src/app/probleme/probleme.component.spec.ts
+++ src/app/probleme/probleme.component.spec.ts
@@ -1,23 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ProblemeComponent } from './probleme.component';
 
-describe('ProblemeComponent', () => {
-  let component: ProblemeComponent;
-  let fixture: ComponentFixture<ProblemeComponent>;
+ describe('ProblemeComponent', () => {
+   let component: ProblemeComponent;
+   let fixture: ComponentFixture<ProblemeComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+   beforeEach(async () => {
+     await TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule],
       declarations: [ ProblemeComponent ]
-    })
-    .compileComponents();
+     })
+     .compileComponents();
 
-    fixture = TestBed.createComponent(ProblemeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+     fixture = TestBed.createComponent(ProblemeComponent);
+     component = fixture.componentInstance;
+     fixture.detectChanges();
+   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+  it('champ prenom invalide avec 2 caractères', ()=> {
+    let prenom = component.problemeForm.get('prenom');
+    prenom.setValue('a'.repeat(2));
+    let errors = prenom.errors || {};
+    expect(errors['minlength']).toBeTruthy();
   });
 });
